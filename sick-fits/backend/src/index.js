1487ba5fddfc509997d213c8f3e1a9479c2e5872
jonathanhoy <jonathanhoy5@gmail.com>
2020-01-13 21:38:36 -0500

    finished module 24

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,10 +1,11 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env'});
 const createServer = require ('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-//TO DO use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 //TO DO use express middleware to populate current user
 
 server.start({
