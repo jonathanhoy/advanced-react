9fce4bb83b27c5871d67a4b66c3856060d10cf10
jonathanhoy <jonathanhoy5@gmail.com>
2020-01-27 20:45:18 -0500

    finished module 42

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -235,6 +235,32 @@ const Mutations = {
       info
     );
   },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `{ id, user { id }}`,
+    );
+    // 1.5 make sure we found an item
+    if (!cartItem) throw new Error('No cart item found');
+    // 2. make sure they own the cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheatin huhhhh');
+    };
+    // 3. delete that cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { 
+          id: args.id
+        },
+      },
+      info,
+    );
+  }
 };
 
 module.exports = Mutations;
