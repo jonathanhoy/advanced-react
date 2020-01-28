4855868e5b33762924784bcb21a9dc314a1163ac
jonathanhoy <jonathanhoy5@gmail.com>
2020-01-27 21:02:31 -0500

    finished module 43

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -27,14 +27,29 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
   };
+  // gets called as soon as we get a response back from server after a mutation has been performed 
+  update = (cache, payload) => {
+    // 1. read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    // 2. remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // 3. write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data});
+  };
   render() {
     return (
       <Mutation
-        variables={{
-          id: this.props.id
-        }}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+        variables={{ id: this.props.id }}
         mutation={REMOVE_FROM_CART_MUTATION}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          },
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton 
