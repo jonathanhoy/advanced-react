4a37fb53fe359b2aab57507ff357cd6d6370ca66
jonathanhoy <jonathanhoy5@gmail.com>
2020-02-09 21:09:31 -0500

    finished module 62

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -28,11 +28,11 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
   state = {
-    title: 'cool shoes',
-    description: 'dummy description',
-    image: 'dog.jpg',
-    largeImage: 'bigdog.jpg',
-    price: 1000,
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0,
   };
 
   handleChange = (e) => {
@@ -42,7 +42,6 @@ class CreateItem extends Component {
   };
   
   uploadFile = async (e) => {
-    console.log('uploading file');
     const files = e.target.files;
     const data = new FormData();
     data.append('file', files[0]);
@@ -52,7 +51,6 @@ class CreateItem extends Component {
       body: data
     });
     const file = await res.json();
-    console.log(file);
     this.setState({
       image: file.secure_url,
       largeImage: file.eager[0].secure_url
@@ -63,20 +61,20 @@ class CreateItem extends Component {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
-          <Form onSubmit={async (e) => {
-            // stop form from submitting
-            e.preventDefault();
-
-            // call the mutation
-            const res = await createItem();
-
-            // change user to single itme page
-            console.log(res);
-            Router.push({
-              pathname: "/item",
-              query: { id: res.data.createItem.id }
-            })            
-          }}>
+          <Form 
+            data-test="form"
+            onSubmit={async (e) => {
+              // stop form from submitting
+              e.preventDefault();
+              // call the mutation
+              const res = await createItem();
+              // change user to single item page
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
+              })            
+            }
+          }>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
               <label htmlFor="file">
